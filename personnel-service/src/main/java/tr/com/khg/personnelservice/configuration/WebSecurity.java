f3a2999f6536e43d759f143863f36eb625a96391
KaanHGunay <kaangunay16@gmail.com>
2020-06-26 19:26:33 +0300

    Spring Security Added

--- personnel-service/src/main/java/tr/com/khg/personnelservice/configuration/WebSecurity.java
+++ personnel-service/src/main/java/tr/com/khg/personnelservice/configuration/WebSecurity.java
@@ -1,7 +1,7 @@
 package tr.com.khg.personnelservice.configuration;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -10,14 +10,18 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 @EnableWebSecurity
 public class WebSecurity extends WebSecurityConfigurerAdapter {
 
-    @Value("${api.ip-address}")
-    private String gatewayIpAddress;
+    private final Environment environment;
+
+    public WebSecurity(Environment environment) {
+        this.environment = environment;
+    }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.csrf().disable();
         http.
-                authorizeRequests().
+            csrf().
+                disable().
+            authorizeRequests().
                 antMatchers("/personnel/**").
                 permitAll();
     }
